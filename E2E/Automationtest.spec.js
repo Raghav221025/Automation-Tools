7bb2101085a0cc8a699e4ee45aaf0bd12676a7d7
Raghav221025 <raghavendhran10@gmail.com>
2025-09-01 20:49:01 +0530

    removed skip and added only

--- E2E/Automationtest.spec.js
+++ E2E/Automationtest.spec.js
@@ -8,7 +8,7 @@ test.describe('verify the automation',()=>{
      Testcases=new playwright(page)
     })
 
-    test.skip("Register User",async({page})=>{
+    test.only("Register User",async({page})=>{
         await Testcases.Homepage("https://automationexercise.com/login")
         await Testcases.RegisterUser();
 
